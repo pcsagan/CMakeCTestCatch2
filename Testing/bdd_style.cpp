40e16df4fbb8ad4cbae06f0a1fe7379fef206a88
pcsagan <pcsagan@gmail.com>
2023-04-09 14:10:17 -0300

    Add testing

---
+++ Testing/bdd_style.cpp
@@ -0,0 +1,25 @@
+#include <catch2/catch_test_macros.hpp>
+#include <string>
+#include <vector>
+
+SCENARIO( "vector can be sized and resized" ) {
+    GIVEN( "An empty vector" ) {
+        auto v = std::vector<std::string>{};
+
+        // Validate assumption of the GIVEN clause
+        THEN( "The size and capacity start at 0" ) {
+            REQUIRE( v.size() == 0 );
+            REQUIRE( v.capacity() == 0 );
+        }
+
+        // Validate one use case for the GIVEN object
+        WHEN( "push_back() is called" ) {
+            v.push_back("hullo");
+
+            THEN( "The size changes" ) {
+                REQUIRE( v.size() == 1 );
+                REQUIRE( v.capacity() >= 1 );
+            }
+        }
+    }
+}
